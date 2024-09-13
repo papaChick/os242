0ecef4f1cbb8faeab5541653eec30461447e9ad6
Christian Raphael Heryanto <raphaelheryanto270605@gmail.com>
2024-09-13 22:59:52 +0700

    change script REC2 key

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="6A217FF59826CB8BC81E4402221E76349977820D"
+REC2="221E76349977820D"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
